6a6169a1576f7354a2a64bb7c0c3f721b410099e
dbrodie122 <david.brodie122@gmail.com>
2017-02-08 15:36:37 -0800

    Implement functional-shared pattern stack.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  //var size = 0;
+  var instance = {};
+  instance.count = 0;
+  instance.storage = {};
+  _.extend(instance, stackMethods);
+  return instance;
 };
 
 var stackMethods = {};
+stackMethods.push = function (value) {
+  this.count++;
+  this.storage[this.count] = value;
+};
+stackMethods.pop = function() {
+  var popped = this.storage[this.count];
+  if (this.count > 0) {
+    this.count--;
+  }
+  return popped;
+};
+stackMethods.size = function() {
+  return this.count;
+};
 
 
--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,8 @@
 var Queue = function() {
   var someInstance = {};
+  var size = 0;
+  var enqueueIndex = 0;
+  var dequeueIndex = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,12 +10,21 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    size++;
+    enqueueIndex++;
+    storage[enqueueIndex] = value;
   };
 
   someInstance.dequeue = function() {
+    if (size > 0) {
+      size--;
+    }
+    dequeueIndex++;
+    return storage[dequeueIndex];
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
